7404c40b7eecc80c362203975d76566dbd0e2b67
Stuart Stevenson <stuartjackstevenson@gmail.com>
2013-08-31 21:21:14 +0100

    dummy data load

--- src/main/java/com/rightmove/es/repositories/DummyCSVRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rightmove.es.repositories;
-
-import com.rightmove.es.domain.DummyCSVRecord;
-import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
-
-public interface DummyCSVRecordRepository extends ElasticsearchCrudRepository<DummyCSVRecord, String> {
-}
