5bd2760e3488db45331c34cc36f83b82052cd8cd
carloshenrique153@gmail.com <carloshenrique153@gmail.com>
2013-09-05 15:57:47 +0100

    ignoring test for random property indexing

--- src/test/java/com/rightmove/es/randomdata/RandomPropertyGeneratorTest.java
+++ src/test/java/com/rightmove/es/randomdata/RandomPropertyGeneratorTest.java
@@ -2,6 +2,7 @@ package com.rightmove.es.randomdata;
 
 import java.util.ArrayList;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +20,11 @@ public class RandomPropertyGeneratorTest {
 	private PropertyService propertyService;
 
 	@Test
+	@Ignore("should not be part of the build")
 	public void indexRandomProperties() throws Exception {
-		for (int i = 0; i < 4000; i++) {
+		for (int i = 0; i < 40000; i++) {
 			ArrayList<Property> properties = new ArrayList<Property>();
-			for (long j = 1; j < 2000; j++) {
+			for (long j = 1; j < 100; j++) {
 				Property property = (Property) new RandomObjectGenerator().fill(Property.class, 2);
 				properties.add(property);
 			}
